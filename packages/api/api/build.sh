013739165b0f143e38c508b5947f34b78cd815ed
Alejandro Soto <asoto@aec.co.cr>
2024-08-01 21:43:51 -0600

    Update build.sh

--- packages/api/api/build.sh
+++ packages/api/api/build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-virtualenv --without-pip virtualenv
-pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
+sudo virtualenv --without-pip virtualenv
+sudo pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
